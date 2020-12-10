8eb66daf40f819fde4f62b79ed0fd9a8f41c2443
lingyia <42825321+lingyia@users.noreply.github.com>
2020-12-10 23:09:17 +0800

    Add files via upload

---
+++ 2020.12.10/233.js
@@ -0,0 +1,7 @@
+{
+  "hotversion": "4.4",
+  "version": "4.4",
+  "injection": "<p style=\"text-align:right;width:100%;margin:0;\"><a >淘宝购物不用券？你out了！来这里，每年省下一个亿！</a>|<a href=\"https://cdn.jsdelivr.net/gh/lingyia/webimg/2020.12.10/%E6%8E%A8%E5%B9%BF%E6%B5%B7%E6%8A%A5-%E6%A8%A1%E6%9D%BF2.png\" target='_blank'>加入QQ群</a></p>\n<p style=\"text-align:right;width:100%;margin:0;\">[<a href=\"https://cdn.jsdelivr.net/gh/lingyia/webimg/2020.12.10/%E6%8E%A8%E5%B9%BF%E6%B5%B7%E6%8A%A5-%E6%A8%A1%E6%9D%BF2.png\" target='_blank' style=\"color:red;font-weight: bold;\">淘宝优惠券</a>]</p>",
+  "enforce": false,
+  "url": "https://github.com/lingyia"
+}
